5433622a225abe4166c9021de2762411c1c2465e
jasonjjl1999 <43978361+jasonjjl1999@users.noreply.github.com>
2019-03-14 16:56:17 -0400

    Formatting

--- chess.py
+++ chess.py
@@ -2,17 +2,17 @@ from chessPlayer import *
 
 board = genBoard()
 
-#Move Maker
+# Move Maker
 done = False
 while done == False:
     print("The game will now begin")
     printBoard(board)
-    
+
     while True:
-        print (chessPlayer(board, 1))
+        print(chessPlayer(board, 1))
         printBoard(board)
-        white_index = int(raw_input("White: Enter the index of the piece to move "))
-        if (board[white_index]>15 or board[white_index]<10):
+        white_index = int(input("White: Enter the index of the piece to move "))
+        if (board[white_index] > 15 or board[white_index] < 10):
             print("There is no valid white piece at the index")
         else:
             movelist = GetPieceLegalMoves(board, white_index)
@@ -21,11 +21,11 @@ while done == False:
             else:
                 piece_id = board[white_index]
                 break
-        
+
     while True:
         movelist = GetPieceLegalMoves(board, white_index)
         print(movelist)
-        white_move = int(raw_input("White: Enter the index to move to "))
+        white_move = int(input("White: Enter the index to move to "))
         for i in movelist:
             if white_move == i:
                 board[white_index] = 0
@@ -35,12 +35,13 @@ while done == False:
             break
         else:
             print("Invalid move")
-            
+
     while True:
         print(chessPlayer(board, 2))
         printBoard(board)
-        black_index = int(raw_input("Black: Enter the index of the piece to move "))
-        if (board[black_index]>25 or board[black_index]<20):
+        black_index = int(
+            raw_input("Black: Enter the index of the piece to move "))
+        if (board[black_index] > 25 or board[black_index] < 20):
             print("There is no valid black piece at the index")
         else:
             movelist = GetPieceLegalMoves(board, black_index)
@@ -49,11 +50,11 @@ while done == False:
             else:
                 piece_id = board[black_index]
                 break
-        
+
     while True:
         movelist = GetPieceLegalMoves(board, black_index)
         print(movelist)
-        black_move = int(raw_input("Black: Enter the index to move to "))
+        black_move = int(input("Black: Enter the index to move to "))
         for i in movelist:
             if black_move == i:
                 board[black_index] = 0
